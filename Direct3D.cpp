dd6642907ac8c5ae5d7b88d4a4e864d6223e5393
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-03-05 09:07:44 +1000

    Add files via upload

---
+++ Direct3D.cpp
@@ -0,0 +1,38 @@
+public class Direct3D;
+public class Neurons;
+operator Neurons;
+friend Neurons;
+register Neurons;
+{
+	//Windows Direct3D optimized
+	//Windows Direct3D shaders compiled
+	//Windows Direct3D usage shaders
+	//Windows Direct3D usage HDR
+	//Windows Direct3D usage Bloom
+	//Windows Direct3D usage Ultra hight dynamic range
+    propertyName Direct3D; propertyName Windows; propertyName Neurons
+}
+1: void foo(){ 
+2: int x; 
+3: for(int i = 0; i < N; i++) 
+4: x += i; 
+5: }
+void foo(){ 
+int x; 
+for(int i = 0; i < N; i++) 
+x += bar(i); 
+}
+int bar(int i){ 
+return i; 
+}
+for(int i = 0; i < N; i++) 
+*a++ = *b++ + *c++; 
+for(int i = 0; i < N; i++) 
+a[i] = b[i] + c[i]; 
+a += N; 
+b += N; 
+c += N;
+for(int i = 0; i < N; i++) 
+a[i] = b[i] + c[i];
+
+
