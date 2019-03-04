76914442fdeba7a716d3bd89b34dbbbe98088134
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-03-05 09:05:53 +1000

    Add files via upload

---
+++ directx.cpp
@@ -0,0 +1,37 @@
+public class DirectX;
+public class Neurons;
+operator Neurons;
+friend Neurons;
+register Neurons;
+{
+	//Windows DirectX optimized
+	//Windows DirectX shaders compiled
+	//Windows DirectX usage shaders
+	//Windows DirectX usage HDR
+	//Windows DirectX usage Bloom
+	//Windows DirectX usage Ultra hight dynamic range
+    propertyName DirectX; propertyName Windows; propertyName Neurons
+}
+1: void foo(){ 
+2: int x; 
+3: for(int i = 0; i < N; i++) 
+4: x += i; 
+5: }
+void foo(){ 
+int x; 
+for(int i = 0; i < N; i++) 
+x += bar(i); 
+}
+int bar(int i){ 
+return i; 
+}
+for(int i = 0; i < N; i++) 
+*a++ = *b++ + *c++; 
+for(int i = 0; i < N; i++) 
+a[i] = b[i] + c[i]; 
+a += N; 
+b += N; 
+c += N;
+for(int i = 0; i < N; i++) 
+a[i] = b[i] + c[i]; 
+
