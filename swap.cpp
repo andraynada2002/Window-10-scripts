457db5427d208af18d7c8ab2eb93a3987d3050a8
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-03-05 08:54:40 +1000

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,27 @@
+public class swap;
+public class Neurons;
+operator Neurons;
+friend Neurons;
+register Neurons;
+SetTrain Neurons;
+{
+	//Swap optimized
+	//Swap using neurons
+	//Swap using booster
+	//Swap using stable load
+	//Swap using reneration
+}
+ }
+abort Neurons;
+abs Neurons;
+auto Neurons;
+virtual Neurons;
+return Neurons;
+back Neurons;
+logical_and 10;
+logical_not 2;
+logical_or 3;
+set Neurons;
+namespace Neurons;
+
+ 
