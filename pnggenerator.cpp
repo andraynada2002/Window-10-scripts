899812e8b803f5a5233f4967e190f0879dd12ab8
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-03-06 10:24:17 +1000

    Add files via upload

---
+++ pnggenerator.cpp
@@ -0,0 +1,48 @@
+1: void foo(){ 
+2: int x; 
+3: for(int i = 0; i < N; i++) 
+4: x += i; 
+5: }
+void foo(){ 
+int x; 
+for(int i = 0; i < N; i++) 
+x += bar(i); 
+}
+int bar(int i){ 
+return i; 
+}
+for(int i = 0; i < N; i++) 
+*a++ = *b++ + *c++; 
+for(int i = 0; i < N; i++) 
+a[i] = b[i] + c[i]; 
+a += N; 
+b += N; 
+c += N;
+for(int i = 0; i < N; i++) 
+a[i] = b[i] + c[i];
+public class PNGgenerator;
+public class Neurons;
+operator Neurons;
+friend Neurons;
+register Neurons;
+{
+	//PNG image quality best
+	//PNG image quality normal
+	//PNG image quality Good + Best
+	//PNG image fixed and remove artifacts
+	//PNG image fixed and remove noise
+	//PNG image remove chromatic aberration
+	propertyName PNG; propertyName image; propertyName quality;
+	propertyName normal;
+	propertyName best; propertyName Fixed;
+	propertyName Remove; propertyName noise; propertyName noise; propertyName ChromaticAberration;
+}
+abort Neurons;
+abs Neurons;
+absread Neurons;
+abswrite Neurons;
+asm Neurons;
+if Neurons;
+else Neurons;
+
+
