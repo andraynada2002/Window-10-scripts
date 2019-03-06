9deedfa2cfa4e83911ce31e13445994e87d02375
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-03-06 10:43:00 +1000

    Add files via upload

---
+++ avigenerator.cpp
@@ -0,0 +1,47 @@
+1: void foo(){ 
+2: int x; 
+3: for(int i = 0; i < N; i++) 
+4: x += i; 
+5: }
+void foo(){ 
+int x; 
+for(int i = 0; i < N; i++) 
+x += bar(i); 
+}
+int bar(int i){ 
+return i; 
+}
+for(int i = 0; i < N; i++) 
+*a++ = *b++ + *c++; 
+for(int i = 0; i < N; i++) 
+a[i] = b[i] + c[i]; 
+a += N; 
+b += N; 
+c += N;
+for(int i = 0; i < N; i++) 
+a[i] = b[i] + c[i];
+public class AVIgenerator;
+public class Neurons;
+operator Neurons;
+friend Neurons;
+register Neurons;
+{
+ //AVI video optimized
+ //AVI video boosted
+ //AVI video realtime performance
+ //AVI video fixed and remove artifacts
+ //AVI video fixed and remove chromatic aberration
+ //AVI video fixed and remove noise
+ //AVI video quality Good + Best
+ //AVI video realtime recording
+ //AVI video realtime framerate
+ //AVI video realtime fps
+ propertyName AVI; propertyName Video; propertyName realtime; propertyName recording; propertyName ChromaticAberration; propertyName Quality; propertyName Framerate; propertyName FPS;
+}
+abort Neurons;
+abs Neurons;
+absread Neurons;
+abswrite Neurons;
+asm Neurons;
+if Neurons;
+else Neurons;
---
+++ mp4generator.cpp
@@ -0,0 +1,48 @@
+1: void foo(){ 
+2: int x; 
+3: for(int i = 0; i < N; i++) 
+4: x += i; 
+5: }
+void foo(){ 
+int x; 
+for(int i = 0; i < N; i++) 
+x += bar(i); 
+}
+int bar(int i){ 
+return i; 
+}
+for(int i = 0; i < N; i++) 
+*a++ = *b++ + *c++; 
+for(int i = 0; i < N; i++) 
+a[i] = b[i] + c[i]; 
+a += N; 
+b += N; 
+c += N;
+for(int i = 0; i < N; i++) 
+a[i] = b[i] + c[i];
+public class MP4generator;
+public class Neurons;
+operator Neurons;
+friend Neurons;
+register Neurons;
+{
+ //MP4 video optimized
+ //MP4 video boosted
+ //MP4 video realtime performance
+ //MP4 video fixed and remove artifacts
+ //MP4 video fixed and remove chromatic aberration
+ //MP4 video fixed and remove noise
+ //MP4 video quality Good + Best
+ //MP4 video realtime recording
+ //MP4 video realtime framerate
+ //MP4 video realtime fps
+ propertyName MP4; propertyName Video; propertyName realtime; propertyName recording; propertyName ChromaticAberration; propertyName Quality; propertyName Framerate; propertyName FPS;
+}
+abort Neurons;
+abs Neurons;
+absread Neurons;
+abswrite Neurons;
+asm Neurons;
+if Neurons;
+else Neurons;
+
