f71f9d9372a9bb2e4e80ace3f1f86a1e6f5ea453
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-03-12 20:10:32 +1000

    Add files via upload

---
+++ windowsos.cpp
@@ -0,0 +1,28 @@
+class windowsos();
+if windowsos();
+else windowsos();
+else {
+	//Windows os trained
+	//Windows os optimization trained
+	//Windows os performance trained
+	//Windows os CPU performance trained
+	//Windows os GPU performance trained
+	//Windows os RAM performance trained 
+	//Windows os Display performance trained
+	//Windows os CPU optimization trained
+	//Windows os GPU optimization trained
+	//Windows os RAM optimization trained
+	//Windows os Display optimization trained
+	//Windows os audio trained
+	//Windows os video trained
+	//Windows os image trained
+	//Windows os image quality trained
+	//Windows os image definition trained
+	//Windows os video quality trained
+	//Windows os video definition trained
+	//Windows os web image trained
+	//Windows os web image quality trained
+	//Windows os web image definition trained
+	//Windows os web video quality trained
+	//Windows os web video definition trained
+}();
