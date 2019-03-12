18a268b59aed51e614b220dbbf6902e4bbad7835
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-03-12 20:04:24 +1000

    Add files via upload

---
+++ neuronet.cpp
@@ -0,0 +1,32 @@
+class neuronet();
+if neuronet();
+else neuronet():
+else {
+ //Wi-Fi connection trained
+ //Wi-Fi speed trained
+ //Wi-Fi trained
+ //Ethernet connection trained
+ //Ethernet speed trained
+ //Ethernet trained
+ //Net Speed trained
+ //Net connection trained
+ //Net trained
+ //Wi-Fi DNS trained
+ //Ethernet DNS trained
+ //Net DNS trained
+ //Wi-Fi performance trained
+ //Ethernet performance trained
+ //Net performance trained
+ //Wi-Fi speed performance trained
+ //Wi-Fi connection performance trained
+ //Ethernet speed performance trained
+ //Ethernet connection performance trained
+ //Net speed performance trained
+ //Net connection speed trained
+}();
+
+
+
+
+
+
