96739601ebe0378dac8e8adff5b110f4e8375b7b
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-03-20 17:56:44 +1000

    Add files via upload

---
+++ windows10.cs
@@ -0,0 +1,38 @@
+public class windows10(class.set);
+class windows10(class.set);
+request.AddParam(set.params("Windows_10_optimized", data);
+request.AddParam(set.params("Windows_10_framerate", 144);
+request.AddParam(set.params("Windows_10_set_performance", 100%);
+request.AddParam(set.params("Windows_10_ram_set_performance", 100%);
+request.AddParam(set.params("Windows_10_cpu_set_performance", 100%);
+class wifi(class.set);
+performance.AddParam(set.performance);
+performance.AddParam(set.params);
+request.AddParam(set.params)
+request.AddParam(set.windows10_params);
+request.AddParam(set.params("windows_10_optimizer");
+request.AddParam(set.params("Windows_10_boosted");
+request.AddParam(set.params("Windows_10_framerate", 144);
+request.AddParam(set.params("Windows_10_wifi_signal", hight);
+request.AddParam(set.params("Windows_10_wifi_signal", autohight);
+request.AddParam(set.params("Windows_10_wifi_speed", ultrahight);
+request.AddParam(set.params("Windows_10_wifi_speed", autoultrahight);
+request.AddParam(set.params("Windows_10_wifi_qos_bandwidth", 5Ghz);
+request.AddParam(set.params("Windows_10_wifi_qos_bandwidth", auto5ghz);
+request.AddParam(set.params("Windows_10_wifi_duplex_speed", ultrahight);
+request.AddParam(set.params("Windows_10_wifi_duplex_speed", autohight);
+request.AddParam(set.params("Windows_10_wifi_channel" 7);
+request.AddParam(set.params("Windows_10_wifi_width_channel" 7);
+request.AddParam(set.params("Windows_10_wifi_signal_coverage" 10db);
+request.AddParam(set.params("Windows_10_wifi_signal_hight_coverage" 10db);
+request.AddParam(set.params("Windows_10_wifi_signal_hight_saving");
+request.AddParam(set.params("Windows_10_wifi_signal_5_strips");
+request.AddParam(set.params("Windows_10_wifi_ports", infinite);
+request.AddParam(set.params("Windows_10_wifi_tcp", optimized);
+request.AddParam(set.params("Windows_10_ethernet_tcp", optimized);
+
+
+
+
+
+
