fd710301a01e8c8a0d97c3aff28535aa5b895fbf
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-06-11 12:02:15 +1000

    Add files via upload

---
+++ opengl3d.cs
@@ -0,0 +1,31 @@
+//Optimizing OpenGL
+//Optimizing Properties OpenGL
+//Optimizign OpenGL - video
+//Optimizing OpenGL - sound
+//Optimizing Functions OpenGL
+//Optimizing OpenGL - performance
+[System.AttributeUsage(System.AttributeTargets.OpenGL3D, Inherited = false, AllowMultiple = true)]
+sealed class MyAttribute : System.Attribute
+{
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public MyAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+}
+
