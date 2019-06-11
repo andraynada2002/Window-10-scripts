ab409df674ee7e2f1725c0c72bcc269037191d53
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-06-11 13:23:53 +1000

    Add files via upload

---
+++ applications.cs
@@ -0,0 +1,30 @@
+//Optimizing Applications
+//Optimizing Applications - performance
+//Optimizing Appliications - sound
+//Optimizing Applications - video
+//Optimizing Applications - properties
+//Optimizing Applications - functions
+[System.AttributeUsage(System.AttributeTargets.Applications, Inherited = false, AllowMultiple = true)]
+sealed class MyAttribute : System.Attribute
+{
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public MyAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+}
