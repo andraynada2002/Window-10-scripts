17a6404c7a4acee60cabad9bcc4e4f475c191c81
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-06-11 11:58:53 +1000

    Add files via upload

---
+++ directx13.cs
@@ -0,0 +1,35 @@
+//Optimizing DirectSound
+//Optimizing Files DirectX
+//Optimizing DirectX - video
+//Optimizing DirectX - sound
+//Optimizing DirectDraw
+//Optimizing Properties Direct3D
+//Optimizing Properties DirectDraw
+//Optimizing Direct3D
+//Optimizing Functions Direct3D
+//Optimizing FourCC
+[System.AttributeUsage(System.AttributeTargets.DX13, Inherited = false, AllowMultiple = true)]
+sealed class MyAttribute : System.Attribute
+{
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public MyAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+}
+
