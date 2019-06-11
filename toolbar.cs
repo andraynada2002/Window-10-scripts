305c4f83228052ac3709db75a895ca2868ac7e24
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-06-11 13:07:20 +1000

    Add files via upload

---
+++ toolbar.cs
@@ -0,0 +1,30 @@
+//Optimizing Toolbar
+//Optimizing Toolbar - video
+//Optimizing Toolbar - sound
+//Optimizing Toolbar - performance
+//Optimizing Toolbar - functions
+//Optimizing Toolbar - properties
+[System.AttributeUsage(System.AttributeTargets.Toolbar, Inherited = false, AllowMultiple = true)]
+sealed class MyAttribute : System.Attribute
+{
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public MyAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+}
