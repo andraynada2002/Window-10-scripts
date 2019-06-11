ebd845641542d466adc4e07e6668c1e150ec48b2
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-06-11 12:19:54 +1000

    Add files via upload

---
+++ opencl3d.cs
@@ -0,0 +1,28 @@
+//Optimizing OpenCL
+//Optimizing OpenCL - performance
+//Optimizing OpenCL - video
+//Optimizing OpenCL - sound
+[System.AttributeUsage(System.AttributeTargets.OpenCL3D, Inherited = false, AllowMultiple = true)]
+sealed class MyAttribute : System.Attribute
+{
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public MyAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+}
