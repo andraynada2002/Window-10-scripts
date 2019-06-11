52f12169800ea602cdb148162c3931ad9e8c2c93
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-06-11 13:21:40 +1000

    Add files via upload

---
+++ startmenu.cs
@@ -0,0 +1,30 @@
+//Optimizing Start Menu
+//Optimizing Start Menu - performance
+//Optimizing Start Menu - video
+//Optimizing Start Menu - sound
+//Optimizing Start Menu - properties
+//Optimizing Start Menu - functions
+[System.AttributeUsage(System.AttributeTargets.StartMenu, Inherited = false, AllowMultiple = true)]
+sealed class MyAttribute : System.Attribute
+{
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public MyAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+}
