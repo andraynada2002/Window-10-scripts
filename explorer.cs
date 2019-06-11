750301e727c13357cf14e7f4f595bbecf5be1318
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-06-11 13:44:56 +1000

    Add files via upload

---
+++ explorer.cs
@@ -0,0 +1,31 @@
+//explorer.exe
+//Optimizing Explorer
+//Optimizing Explorer - performance
+//Optimizing Explorer - video
+//Optimizing Explorer - sound
+//Optimizing Explorer - properties
+//Optimizing Explorer - functions
+[System.AttributeUsage(System.AttributeTargets.Explorer, Inherited = false, AllowMultiple = true)]
+sealed class MyAttribute : System.Attribute
+{
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public MyAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+}
