e96d6697fc8cb516e9fb7c128c7ef22b806d39d5
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-06-11 12:18:25 +1000

    Add files via upload

---
+++ cuda.cs
@@ -0,0 +1,28 @@
+//Optimizing CUDA
+//Optimizing CUDA - performance
+//Optimizing CUDA - video
+//Optimizing CUDA - sound
+[System.AttributeUsage(System.AttributeTargets.CUDA3D, Inherited = false, AllowMultiple = true)]
+sealed class MyAttribute : System.Attribute
+{
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public MyAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+}
