a468174e75e425be145b84a0bdd29d10aeb469ab
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-06-11 12:43:20 +1000

    Add files via upload

---
+++ videobooster.cs
@@ -0,0 +1,30 @@
+//Optimizing Videobooster
+//Optimizing Videobooster - performance
+//Optimizing Videobooster - video
+//Optimizing Videobooster - sound
+//Optimizing Videobooster - properties
+//Optimizing Videobooster - functions
+[System.AttributeUsage(System.AttributeTargets.Videobooster, Inherited = false, AllowMultiple = true)]
+sealed class MyAttribute : System.Attribute
+{
+    // See the attribute guidelines at
+    //  http://go.microsoft.com/fwlink/?LinkId=85236
+    readonly string positionalString;
+    
+    // This is a positional argument
+    public MyAttribute(string positionalString)
+    {
+        this.positionalString = positionalString;
+        
+        // TODO: Implement code here
+        throw new System.NotImplementedException();
+    }
+    
+    public string PositionalString
+    {
+        get { return positionalString; }
+    }
+    
+    // This is a named argument
+    public int NamedInt { get; set; }
+}
